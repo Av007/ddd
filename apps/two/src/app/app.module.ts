339232aac799f78ca33b32ffd5f97a815b058689
Av007 <avdeevvladimir@gmail.com>
2025-01-25 21:51:12 +0200

    improved config

--- apps/two/src/app/app.module.ts
+++ apps/two/src/app/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './config/configuration';
 import { LogModule } from './log/log.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(configuration().database.url), LogModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+        dbName: configService.get<string>('DATABASE_NAME'),
+      }),
+      inject: [ConfigService],
+    }), 
+    LogModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
